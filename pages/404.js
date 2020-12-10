55b06bbda43f11202cc8e98817f6307f2883f7cd
Magnus Volkmann <magnus@Magnuss-iMac.fritz.box>
2020-12-10 18:56:27 +0100

    fix: build error if post not exists

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import App from '../components/App'
+import Header from '../components/Header'
+
+const FourOFour = () => (
+  <App>
+    <Header />
+    <article>
+      <h1>404</h1>
+    </article>
+  </App>
+)
+
+export default FourOFour
